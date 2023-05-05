2260b0bccd9cea4fda68defee49d4affd56b11fc
Emily Nelson <evnelson2021@gmail.com>
2023-05-05 18:13:07 -0500

    setup

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,7 +1,7 @@
 export const ApplicationViews = () => {
 	return <>
 		<h1 className="title--main">Austin's Tree Co.</h1>
-		<div>Your one-stop shop for repairing your tech</div>
+		<div>Your one-stop shop for tree removing and maintenance</div>
 	</>
 }
 
